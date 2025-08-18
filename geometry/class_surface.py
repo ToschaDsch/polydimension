17cf8638921cfcc5d7750b636e73b4a0f54f762f
ToschaDsch <aukmiit@gmail.com>
2025-08-18 22:13:02 +0200

    drawAll part 0 + cube 3d part 2 + surface normal, center

--- geometry/class_surface.py
+++ geometry/class_surface.py
@@ -1,4 +1,7 @@
+from typing import Any
+
 import numpy
+from numpy import ndarray, dtype, float64
 
 from geometry.class_line import Line
 from geometry.class_point import Point
@@ -8,7 +11,8 @@ class Surface:
     def __init__(self, list_of_points: list[Point] = None):
         self._list_of_points: list[Point] = list_of_points if list_of_points is not None else []
         self.list_of_lines: list[Line] = []
-        self.normal: list[float] = self.get_normal()
+        self.normal: ndarray[tuple[Any]] = self.get_normal()
+        self.center = self.get_center()
 
     @property
     def list_of_points(self) -> list[Point]:
@@ -19,5 +23,11 @@ class Surface:
         self._list_of_points = list_of_points
         self.normal = self.get_normal()
 
-    def get_normal(self) -> list[float]:
-        a: numpy.array() = self.list_of_lines[0].point_0.coord_0
+    def get_normal(self) -> ndarray[tuple[Any, ...], dtype[float64]]:
+        a = self.list_of_lines[0].point_0.coord_0 - self.list_of_lines[0].point_1.coord_0
+        b = self.list_of_lines[1].point_0.coord_0 - self.list_of_lines[1].point_1.coord_0
+        normal = numpy.cross(a, b)
+        return normal/numpy.linalg.norm(normal)
+
+    def get_center(self) -> ndarray[tuple[Any, ...], dtype[float64]]:
+        return numpy.sum(self.list_of_points.coord_0)/len(self.list_of_points)
--- objects/cube_3d.py
+++ objects/cube_3d.py
@@ -5,6 +5,7 @@ import numpy
 
 from geometry.class_line import Line
 from geometry.class_point import Point
+from geometry.class_surface import Surface
 from objects.class_draw_interface import DrawObject
 
 
@@ -31,3 +32,36 @@ class Cube3d(DrawObject):
             for j in range(i+1,len(self.my_points)):
                 if numpy.sum(self.my_points[i].coord_0 - self.my_points[j].coord_0) == 2:
                     self.my_lines.append(Line(point_0=self.my_points[i], point_1=self.my_points[j]))
+
+    def make_surface(self):
+        surface_0 = [self.my_lines[0],
+                   self.my_points[1],
+                   self.my_points[5],
+                   self.my_points[4]]
+        self.my_surfaces.append(Surface(list_of_points=surface_0))
+        surface_1 = [self.my_lines[0],
+                     self.my_points[2],
+                     self.my_points[6],
+                     self.my_points[4]]
+        self.my_surfaces.append(Surface(list_of_points=surface_1))
+        surface_2 = [self.my_lines[0],
+                     self.my_points[1],
+                     self.my_points[3],
+                     self.my_points[2]]
+        self.my_surfaces.append(Surface(list_of_points=surface_2))
+        surface_3 = [self.my_lines[2],
+                     self.my_points[6],
+                     self.my_points[7],
+                     self.my_points[3]]
+        self.my_surfaces.append(Surface(list_of_points=surface_3))
+        surface_4 = [self.my_lines[4],
+                     self.my_points[6],
+                     self.my_points[7],
+                     self.my_points[5]]
+        self.my_surfaces.append(Surface(list_of_points=surface_4))
+        surface_5 = [self.my_lines[1],
+                     self.my_points[3],
+                     self.my_points[7],
+                     self.my_points[6]]
+        self.my_surfaces.append(Surface(list_of_points=surface_5))
+
