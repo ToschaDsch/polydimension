drawAll part 13 bugfix slider rotation rotation matrix bugfix 0

geometry/geometry_functions.py
@@ -33,18 +33,21 @@ def get_rotate_matrix(sin: list[float], cos: list[float], dimensional: int = 3) 
                          [0, sin[2], cos[2]]
                          ]))
     elif dimensional == 4:
+        print("4d matrix")
         i = 0
         r.append(np.array([[cos[i], -sin[i], 0, 0],
                          [sin[i], cos[i], 0, 0],
                          [0, 0, 1, 0],
                          [0, 0, 0, 1]
                          ]))
+
         i =+ 1
         r.append(np.array([[cos[i], 0, -sin[i], 0],
                          [0, 1, 0, 0],
                          [sin[i], 0, cos[i], 0],
                          [0, 0, 0, 1],
                          ]))
+        """
         i =+1
         r.append(np.array([[cos[i], 0, 0, -sin[i]],
                          [0, 1, 0, 0],
@@ -68,7 +71,7 @@ def get_rotate_matrix(sin: list[float], cos: list[float], dimensional: int = 3) 
                          [0, 1, 0, 0],
                          [0, 0, cos[i], -sin[i]],
                          [0, 0, sin[i], cos[i]],
-                         ]))
+                         ]))"""
     result_matrix = np.identity(dimensional, dtype=np.float64)
 
     for r_i in r: